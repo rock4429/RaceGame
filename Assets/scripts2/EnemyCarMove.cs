4b614abc73840c722d2608f3f0458b8d6e5428da
Mohammed Alam <1590083@CEGEPGIM.LCL>
2017-08-19 14:23:57 -0400

    init

---
+++ Assets/scripts2/EnemyCarMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCarMove : MonoBehaviour {
+
+	public float speed = 8f;
+
+	// Use this for initialization
+	void EnemyCarMoveStart() {
+	
+	}
+	
+	// Update is called once per frame
+	void EnemyCarMoveUpdate() {
+
+		transform.Translate (new Vector3(0,1,0) * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/scripts2/carController.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carController : MonoBehaviour {
+
+	public float carSpeed;
+	public float maxPos = 2.2f;
+
+	Vector3 position;
+	public uiManager ui;
+	public AudioManager am;
+
+	bool currntPlatformAndroid = false;
+
+	Rigidbody2D rb;
+
+	void Awake(){
+
+		rb = GetComponent<Rigidbody2D> ();
+
+#if UNITY_ANDROID
+				currntPlatformAndroid = true;
+#else
+        currntPlatformAndroid = false;
+		#endif
+
+
+		am.carSound.Play ();
+	
+	}
+	
+	void Start () {
+
+		position = transform.position;
+
+		if (currntPlatformAndroid == true) {
+			Debug.Log ("Android");
+		} 
+		else {
+			Debug.Log ("Windows");
+		}
+
+
+
+	}
+	
+
+	void Update () {
+
+
+		if (currntPlatformAndroid == true) {
+			//android specific code
+			//TouchMove ();
+			AccelerometerMove();
+		}
+
+		else {
+
+			position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+			position.x = Mathf.Clamp (position.x, -2.2f, 2.2f);
+
+			transform.position = position;
+		}
+
+		
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Enemy Car") {
+			//Destroy (gameObject);
+
+			gameObject.SetActive(false);
+
+			ui.gameOverActivated();
+			am.carSound.Stop();
+
+
+
+
+
+		}
+	}
+
+	void AccelerometerMove(){
+
+		float x = Input.acceleration.x;
+		Debug.Log ("X = " + x);
+
+
+		if (x < -0.1f) {
+			MoveLeft ();
+		} else if (x > 0.1f) {
+			MoveRight ();	
+		} 
+		
+
+	}
+
+
+	void TouchMove(){
+
+		if (Input.touchCount > 0) {
+
+			Touch touch = Input.GetTouch (0);
+
+			float middle = Screen.width / 2;
+
+			if (touch.position.x < middle && touch.phase == TouchPhase.Began) {
+				MoveLeft ();
+			} 
+			else if (touch.position.x > middle && touch.phase == TouchPhase.Began) {
+				MoveRight ();
+			}
+		
+		} 
+
+		
+
+	}
+
+
+	public void MoveLeft(){
+		rb.velocity = new Vector2 (-carSpeed, 0);
+	}
+	
+	public void MoveRight(){
+		
+		rb.velocity = new Vector2 (carSpeed, 0);
+	}
+	
+	
+
+	/*void AccelerometerMove(){
+		float x = Input.acceleration.x;
+		
+		Debug.Log (" X: " + x);
+		
+		if (x < -0.1f) {
+			MoveLeft ();
+		} else if (x > 0.1f) {
+			MoveRight ();
+		} 
+		else {
+			SetVelocityZero();
+		}
+		
+	}*/
+
+	
+
+
+}
